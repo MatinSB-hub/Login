c589e2bb3dd4c0166317eeab0b74290d1020e3a3
MatinSB-hub <matin.sa200@gmail.com>
2025-04-09 17:57:46 +0430

    Comment

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,21 +1,12 @@
-// import React, { useContext, useRef, useState } from "react";
+
 import "./Navbar.css"
 import logo from "../Assets/logo.png"
 import cart_icon from "../Assets/cart_icon.png"
 import { Link } from "react-router-dom";
-// import { ShopContext } from "../../Contex/ShopContext";
 import nav_dropdown from "../Assets/nav_dropdown.jpg"
 
 const Navbar = () => {
 
-    // const [menu,setmenu] = useState("shop")
-    // const {getTotalCartItems} = useContext(ShopContext);
-    // const menuRef = useRef();
-
-    // const dropdown_toggle = (e) =>{
-    //     menuRef.current.classList.toggle("nav-menu-visible")
-    //     e.target.classList.toggle('open');
-    // }
 
     return(
          <div className="navbar">
